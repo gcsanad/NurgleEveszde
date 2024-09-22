687c62c0a6e97683465ef04aa2e79410b6181589
gcsanad <gcsani01@gmail.com>
2024-09-22 20:51:03 +0200

    Merge?

---
+++ NurgleEveszdeWpf/Pizza.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+
+namespace NurgleEveszdeWpf
+{
+    internal class Pizza : Button
+    {
+        string name;
+        string ingredients;
+        int ar;
+        string imageName;
+
+        public Pizza(string[] tomb)
+        {
+            this.name = tomb[0];
+            this.ingredients = tomb[1];
+            this.ar = int.Parse(tomb[2]);
+            this.imageName = tomb[3];
+        }
+
+        public Pizza(string nev)
+        {
+
+            BitmapImage bitimg = new BitmapImage();
+            bitimg.BeginInit();
+            bitimg.UriSource = new Uri(@$".\\pizzaKepek\\{nev}", UriKind.RelativeOrAbsolute);
+            bitimg.EndInit();
+            this.Width = 50;
+            this.Height = 50;
+            this.Background = new ImageBrush(bitimg);
+            this.imageName = nev;
+        }
+
+        public string Name => name;
+        public string Ingredients => ingredients;
+        public int Ar => ar;
+        public string ImageName => imageName;
+    }
+}
---
+++ NurgleEveszdeWpf/Rendeles.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NurgleEveszdeWpf
+{
+    /// <summary>
+    /// Interaction logic for Rendeles.xaml
+    /// </summary>
+    public partial class Rendeles : Window
+    {
+        public Rendeles()
+        {
+            InitializeComponent();
+            ObservableCollection<Pizza> kosar = new();
+            List<string> pizzak = File.ReadAllLines("teszt.txt").Select(x => x).ToList();
+            lbKosar.ItemsSource = kosar;
+            for (int i = 0; i < pizzak.Count; i++)
+            {
+                var pizza = new Pizza(pizzak[i]);
+                lbPizzak.Items.Add(pizza);
+
+            }
+            for (int i = 0; i < lbPizzak.Items.Count; i++)
+            {
+                if (lbPizzak.Items[i] is Pizza)
+                {
+
+                    (lbPizzak.Items[i] as Pizza).Click += (s, e) =>
+                    {
+                        if (s is Pizza)
+                        {
+                            Pizza masolat = new Pizza((s as Pizza).ImageName.ToString());
+                            masolat.Click += (s, e) => kosar.Remove(s as Pizza);
+                            kosar.Add(masolat);
+
+                        }
+                    };
+
+                }
+            }
+        }
+    }
+}
