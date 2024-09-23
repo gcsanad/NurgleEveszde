27528a91a4571aa4fd64eba7fcaebe3720808f71
beke.tamas <beke.tamas@BURG-1E03-13>
2024-09-23 12:45:51 +0200

    plussz cucc

---
+++ NurgleEveszdeWpf/Ingredients.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NurgleEveszdeWpf
+{
+    internal class Ingredients
+    {
+        int id;
+        string name;
+        int available;
+
+
+        public Ingredients(string[] tomb)
+        {
+            this.id = int.Parse(tomb[0]);
+            this.name = tomb[1];
+            this.available = int.Parse(tomb[2]);
+        }
+
+        public int Id  => id;
+        public string Name  => name; 
+        public int Available => available; 
+    }
+}
